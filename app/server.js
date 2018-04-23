671ab462e031d7d80fa3fb9c3a8b3f9ca0b839c2
Mirela <mirelasteve@abv.bg>
2018-04-23 10:42:38 +0300

    add cors

--- app/server.js
+++ app/server.js
@@ -3,19 +3,20 @@ const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
 const data = require('./data');
 const app = express();
-
+var cors = require('cors');
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
 app.use(expressValidator());
-
+app.use(cors());
 require('./config/express').init(app);
 
 app.use((req, res, next) => {
     res.locals.user = req.user || null;
     return next();
+
 });
 
 require('./routes').init(app, data);
-app.listen(3001);
 
+app.listen(3001);
--- seeders/20180414143402-jobapplications.js
+++ seeders/20180414143402-jobapplications.js
@@ -1,27 +1,29 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('jobapplications', [{
-      userId: 2,
-      title: 'Senior NodeJS Developer',
-      firstName: 'John',
-      lastName: 'Doe',
-      cv: 'cv download link',
-      email: 'mmurray@abv.bg',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }, {
-      userId: 2,
-      title: 'Senior Developer',
-      firstName: 'John',
-      lastName: 'Doe',
-      comment: 'added comment',
-      cv: 'cv download link',
-      coverLetter: 'cover letter download link',
-      email: 'mmurray@abv.bg',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }], {});
-  },
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('jobapplications', [{
+            userId: 2,
+            title: 'Senior NodeJS Developer',
+            firstName: 'John',
+            lastName: 'Doe',
+            cv: 'cv download link',
+            email: 'mmurray@abv.bg',
+            jobId: 4,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }, {
+            userId: 2,
+            title: 'Senior Developer',
+            firstName: 'John',
+            lastName: 'Doe',
+            comment: 'added comment',
+            cv: 'cv download link',
+            coverLetter: 'cover letter download link',
+            email: 'mmurray@abv.bg',
+            jobId: 1,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }], {});
+    },
 };
