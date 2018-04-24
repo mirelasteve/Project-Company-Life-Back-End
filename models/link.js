2ec0f71883d3f2e5eb43472544a500f3e3f011c4
DESKTOP-I9AC578\ACER <mario.velyov@gmail.com>
2018-04-24 18:12:22 +0300

    added new column type to links table and migrated to db

--- models/link.js
+++ models/link.js
@@ -17,6 +17,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: true,
     },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
   }, {});
   link.associate = (models) => {
     // associations can be defined here
