8cdfa3182b08375121ed51a2f7b2ee46e911114d
DESKTOP-I9AC578\ACER <mario.velyov@gmail.com>
2018-04-24 12:32:31 +0300

    changed post route for job application

--- app/routes/jobApplications/jobApplications.api.routes.js
+++ app/routes/jobApplications/jobApplications.api.routes.js
@@ -15,10 +15,8 @@ const init = (app, data) => {
         const jobApplications = await controller.getAllJobApplications(id);
         res.send(jobApplications);
     })
-    .post('/jobapplications/:id', async (req, res) => {
-        const jobId = req.params.id;
+    .post('/jobapplications', async (req, res) => {
         const newJobApplication = req.body;
-        newJobApplication.jobId = jobId;
         await controller.create(newJobApplication);
         res.status(201).send('Job Application added!');
     })
